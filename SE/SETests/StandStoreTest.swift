026b1e1a274f938eaa28aa2fc863b5dd33c2320d
Mastere 1 IT <mastere1it@iMacdeAutres4.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-24 15:17:44 +0200

    save ok start test

---
+++ SE/SETests/StandStoreTest.swift
@@ -0,0 +1,40 @@
+//
+//  StandStoreTest.swift
+//  SE
+//
+//  Created by Mastere 1 IT on 24/06/2016.
+//  Copyright © 2016 Mastere 1 IT. All rights reserved.
+//
+/*
+import XCTest
+
+class StandStoreTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func insert_test() {
+        
+        var store = StandStore()
+        
+        store.insertNewStand("storeTest1")
+        
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func remove_test () {
+        var store = StandStore()
+        
+        store.removeStrand(1)
+    }
+
+}
+*/
