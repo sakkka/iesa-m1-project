0bd24ab848d8d308a4a662da508124d471b1bfaf
Mastere 1 IT <mastere1it@iMacdeAutres45.fro-206.froment.in.iesanetwork.com>
2016-06-27 18:53:51 +0200

    launch screen image

--- SE/SE/AppDelegate.swift
+++ SE/SE/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        NSThread.sleepForTimeInterval(2) //add 2 seconds longer.
+        
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
